fc45398a819ad3f9d789c1b6855d33c59cfdf03d
Nadav <nadav.matalon@gmail.com>
2014-07-08 16:40:20 +0100

    added sign up and sign in links and tests

--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -3,17 +3,51 @@ require 'rails_helper'
 describe 'CoffeeSpots User features' do
   context 'New user page' do
     it 'should have a descriptive header' do
-      visit '/users/new'
-      expect(page).to have_content('Create a New User')
-    end
-  end
-
-  context 'Existing users page' do
-    before{User.create name: 'Roi', email: 'test@test.com'}
-    it 'should display all users' do
-      visit '/users'
-      expect(page).to have_content('test@test.com')
+      visit new_user_registration_path
+      expect(page).to have_content('Sign up')
     end
   end
 
 end
+
+describe 'users registration/login' do
+
+  it 'can sign up' do
+    visit '/users/sign_up'
+    fill_in 'Email', with: 'alex@example.com'
+    fill_in 'Password', with: '12345678'
+    fill_in 'Password confirmation', with: '12345678'
+    click_button 'Sign up'
+    expect(page).to have_content 'Signed up successfully'
+  end
+
+  it 'can sign in' do
+    User.create(email: 'alex@example.com', password: '12345678', password_confirmation: '12345678')
+    visit '/users/sign_in'
+    fill_in 'Email', with: 'alex@example.com'
+    fill_in 'Password', with: '12345678'
+    click_button 'Sign in'
+    expect(page).to have_content 'Signed in successfully'
+  end
+
+  it 'can log out' do
+    User.create(email: 'alex@example.com', password: '12345678', password_confirmation: '12345678')
+    visit '/users/sign_in'
+    fill_in 'Email', with: 'alex@example.com'
+    fill_in 'Password', with: '12345678'
+    click_button 'Sign in'
+    expect(page).not_to have_link('sign in')
+    #visit '/'
+    click_link('log out')
+    expect(page).to have_link('sign in')
+  end
+
+  it 'when logged out' do
+    visit '/'
+    expect(page).not_to have_link('log out')
+    expect(page).to have_link('sign in')
+    expect(page).to have_link('sign up')
+  end
+
+end
+
